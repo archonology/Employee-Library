8393b29a55d9a0688eb6a1e97d6cb713b376d63d
Your Name <you@example.com>
2022-09-23 12:52:47 -0500

    dotenv added and server.js structure

---
+++ server.js
@@ -0,0 +1,34 @@
+//dependencies
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//connect to mySQL database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    //utilize the .env for password secrecy
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+//error catching for middleware
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  //listening port for testing
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
